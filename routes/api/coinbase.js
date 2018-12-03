47d1a7bd9a1081c7fed22fe1d8869373fb791f50
Dylan Ellison <dylancellison@gmail.com>
2018-12-02 21:48:01 -0600

    Removed comments

--- routes/api/coinbase.js
+++ routes/api/coinbase.js
@@ -2,59 +2,21 @@ const axios = require('axios');
 
 module.exports = app => {
 
-    let json = null;
-    const coinList = ['btc', 'eth', 'ltc', 'bch']
+  let json = null;
+  const coinList = ['btc', 'eth', 'ltc', 'bch']
 
-    app.get('/api/coinbase/coins', (req, res) => {
-      const coinUrls = coinList.map(coin => {
-        return `https://api.pro.coinbase.com/products/${coin}-usd/ticker`
-      })
-
-      axios.all(coinUrls.map(l => axios.get(l))).then(axios.spread(function(...res) {
-        return res.map((item, key) => {
-          item.data.coin = coinList[key]
-          return item.data
-        })
-      })).then(fun => {
-        res.send(fun)
-      })
-
-
+  app.get('/api/coinbase/coins', (req, res) => {
+    const coinUrls = coinList.map(coin => {
+      return `https://api.pro.coinbase.com/products/${coin}-usd/ticker`
     })
 
+    axios.all(coinUrls.map(l => axios.get(l))).then(axios.spread((...res) => {
+      return res.map((item, key) => {
+        item.data.coin = coinList[key]
+        return item.data
+      })
+    })).then(prices => {
+      res.send(prices)
+    })
+  })
 }
-
-
-
-
-    // axios.get('https://api.pro.coinbase.com/products/btc-usd/ticker')
-    //   .then(function (response) {
-    //     console.log("The response is ", response.data);
-    //     json = response.data
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //     res.send(error);
-    //   });
-    //
-    // app.get('/api/coinbase/btc-usd', (req, res) => {
-    //   res.send(json);
-    //
-    //
-    // })
-    //
-    // app.get('/api/coinbase/products', (req, res) => {
-    //   axios.get('https://api.pro.coinbase.com/products')
-    //     .then(function (response) {
-    //       console.log("The response is ", response.data);
-    //       products = response.data
-    //       res.send(products);
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //       res.send(error);
-    //     });
-    //
-    //
-    //
-    // })
