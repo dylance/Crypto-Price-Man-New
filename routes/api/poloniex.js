231391e55536338f6fa9e26428f1d429a40345d1
Dylan Ellison <dylancellison@gmail.com>
2018-12-09 15:13:06 -0600

    Made poloniex object properties same as other endpoints

--- routes/api/poloniex.js
+++ routes/api/poloniex.js
@@ -1,16 +1,24 @@
 const axios = require('axios');
+const { swapProperty, multiSwap }= require('../../utils/apiUtils')
 
 module.exports = app => {
+
+  const swapData = [['price', 'last'],['high', 'high24hr'],['low', 'low24hr'],['volume', 'quoteVolume']]
+
+  function compare(a,b) {
+      a = parseInt(a.price);
+      b = parseInt(b.price);
+
+    if (a > b)
+      return -1;
+    if (a < b)
+      return 1;
+    return 0;
+  }
 
   app.get('/api/poloniex/coins-ticker', (req,res) => {
     axios.get('https://poloniex.com/public?command=returnTicker')
       .then((response) => {
-        // for( let coin in response.data) {
-        //   console.log("the coin is", coin)
-        // }
-        // console.log("the object is: ", response.data.BTC_BCN )
-        // BTC_BCN
-
         const props = Object.keys(response.data)
         let entries = Object.values(response.data)
         entries.map((coin, key) => {
@@ -22,6 +30,13 @@ module.exports = app => {
             console.log(coin.coin.indexOf('USDT_'))
            return coin.coin.indexOf("USDT_")  === 0
          })
+
+        entries.forEach((coin, index) => {
+          coin.coin = coin.coin.replace('USDT_', '')
+          multiSwap(coin, swapData);
+        })
+
+        entries = entries.sort(compare);
 
         res.send(entries)
       })
