8b909429eaa337d162596660ccde4c3af94452e2
Dylan Ellison <dylancellison@gmail.com>
2018-12-09 00:15:25 -0600

    Added util functin to swap multiple oject properties

--- utils/apiUtils.js
+++ utils/apiUtils.js
@@ -1,6 +1,13 @@
 module.exports = {
+
   swapProperty: (obj, newProp, oldProp) => {
     obj[newProp] = obj[oldProp];
     delete obj[oldProp];
-  }  
+  },
+
+  multiSwap: (obj, swapVars) => {
+    swapVars.forEach(vars => {
+      module.exports.swapProperty(obj, vars[0], vars[1])
+    })
+  }
 }
