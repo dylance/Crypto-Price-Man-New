ec602374d2d51ae0d9a375fab9ad238d0af16f85
Dylan Ellison <dylancellison@gmail.com>
2018-12-11 22:06:23 -0600

    update poloniex prices on every 10 seconds

--- routes/api/poloniex.js
+++ routes/api/poloniex.js
@@ -1,44 +1,50 @@
-const axios = require('axios');
-const { swapProperty, multiSwap }= require('../../utils/apiUtils')
+const axios = require("axios");
+const { multiSwap } = require("../../utils/apiUtils");
 
 module.exports = app => {
+  const swapData = [
+    ["price", "last"],
+    ["high", "high24hr"],
+    ["low", "low24hr"],
+    ["volume", "quoteVolume"]
+  ];
 
-  const swapData = [['price', 'last'],['high', 'high24hr'],['low', 'low24hr'],['volume', 'quoteVolume']]
+  function compare(a, b) {
+    a = parseInt(a.price);
+    b = parseInt(b.price);
 
-  function compare(a,b) {
-      a = parseInt(a.price);
-      b = parseInt(b.price);
-
-    if (a > b)
-      return -1;
-    if (a < b)
-      return 1;
+    if (a > b) return -1;
+    if (a < b) return 1;
     return 0;
   }
 
-  app.get('/api/poloniex/coins-ticker', (req,res) => {
-    axios.get('https://poloniex.com/public?command=returnTicker')
-      .then((response) => {
-        const props = Object.keys(response.data)
-        let entries = Object.values(response.data)
+  let prices = [];
+
+  setInterval(() => {
+    axios
+      .get("https://poloniex.com/public?command=returnTicker")
+      .then(response => {
+        const props = Object.keys(response.data);
+        let entries = Object.values(response.data);
         entries.map((coin, key) => {
-          coin.coin = props[key]
-          return coin
-        })
+          coin.coin = props[key];
+          return coin;
+        });
 
         entries = entries.filter((coin, key) => {
-            console.log(coin.coin.indexOf('USDT_'))
-           return coin.coin.indexOf("USDT_")  === 0
-         })
+          return coin.coin.indexOf("USDT_") === 0;
+        });
 
         entries.forEach((coin, index) => {
-          coin.coin = coin.coin.replace('USDT_', '')
+          coin.coin = coin.coin.replace("USDT_", "");
           multiSwap(coin, swapData);
-        })
+        });
 
-        entries = entries.sort(compare);
+        prices = entries.sort(compare);
+      });
+  }, 10000);
 
-        res.send(entries)
-      })
-  })
-}
+  app.get("/api/poloniex/coins-ticker", (req, res) => {
+    res.send(prices);
+  });
+};
