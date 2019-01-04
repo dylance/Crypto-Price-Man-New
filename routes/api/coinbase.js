a5e9ae9ec2f43cba30e8ce9e126d9e59130149a5
Dylan Ellison <dylancellison@gmail.com>
2019-01-03 21:31:39 -0600

    Round prices for coinbase

--- routes/api/coinbase.js
+++ routes/api/coinbase.js
@@ -1,16 +1,19 @@
 const axios = require('axios');
+const { roundDecimals }= require('../../utils/apiUtils')
 
 module.exports = app => {
 
   let json = null;
-  const coinList = ['btc', 'eth', 'ltc', 'bch']
-  let pricess = "this is my prices"
+  const coinList = ['btc', 'eth', 'ltc', 'bch', 'etc']
+  let pricess = []
   const coinUrls = coinList.map(coin => `https://api.pro.coinbase.com/products/${coin}-usd/ticker`)
 
   setInterval( () => {  axios.all(coinUrls.map(url => axios.get(url)))
         .then(response => {
           return response.map((item, key) => {
-            item.data.coin = coinList[key]
+            item.data.coin = coinList[key];
+            item.data.price = roundDecimals(item.data.price);
+            item.data.volume = roundDecimals(item.data.volume);
             return item.data
           })
         })
@@ -27,21 +30,23 @@ module.exports = app => {
 
     axios.all(coinUrls.map(url => axios.get(url))).then(axios.spread((...res) => {
       return res.map((item, key) => {
-        item.data.coin = coinList[key]
+        item.data.coin = coinList[key];
+        item.data.high = roundDecimals(item.data.high);
+        item.data.low = roundDecimals(item.data.low);
         return item.data
       })
     })).then(prices => {
       res.send(prices)
-    })
+    }).catch((error) => {
+      console.log("The error is:", error)
+    });
   })
 
   app.get('/api/coinbase/everything', (req, res) => {
     const coinUrls = coinList.map(coin => `https://api.pro.coinbase.com/products/${coin}-usd/stats`)
     const coinUrls2 = coinList.map(coin => `https://api.pro.coinbase.com/products/${coin}-usd/ticker`)
     const coinUrls3 = coinUrls2.concat(coinUrls);
-    console.log("coinurls3 lenght is:", coinUrls3.constructor === Array)
-      axios.all(coinUrls3.map(url => axios.get(url))).then(axios.spread((...response)=> {
-        console.log(response[0])
+    axios.all(coinUrls3.map(url => axios.get(url))).then(axios.spread((...response)=> {
         return response.map((item, key) => {
           item.data.coin = coinList[key]
           return item.data
