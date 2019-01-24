46a35a8b039795deb1f26acd5f61dd0816083b50
Dylan Ellison <dylancellison@gmail.com>
2019-01-23 23:50:07 -0600

    Clean up shipit file

--- shipitfile.js
+++ shipitfile.js
@@ -25,9 +25,7 @@ module.exports = shipit => {
 
   shipit.blTask("copy_keys", function copy_keys() {
     return shipit.remote(
-      `cp /var/www/keys/keys.js ${
-        shipit.releasePath
-      }/config/keys.js`
+      `cp /var/www/keys/keys.js ${shipit.releasePath}/config/keys.js`
     );
   });
 
@@ -35,27 +33,17 @@ module.exports = shipit => {
     return shipit.remote(`forever stopall`);
   });
 
-  // shipit.blTask("start_server", function start_server() {
-  //   return shipit.remote(
-  //     `forever start /var/www/cryptopriceman.com/current/index.js`
-  //   );
-  // });
-
   shipit.on("updated", function start_build() {
     return shipit.start(
-      //'create_cached_directories',
-      //'copy_cached_directories_to_workspace',
       "npm_install",
       "install_client",
       "copy_keys",
       "stop_server"
-      //'copy_build_files_for_cache',
     );
   });
 
   shipit.on("deployed", function clear_caches() {
-    shipit.remote(
-      "cd /var/www/cryptopriceman.com/current && forever start index.js"
-    );
+    shipit.remote(`cd ${shipit.releasePath}/ && forever start index.js`);
+    shipit.remote("systemctl restart nginx");
   });
 };
