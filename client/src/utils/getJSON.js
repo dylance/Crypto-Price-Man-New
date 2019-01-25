7c6488296d769910be8a701c85ccf75edbb781cb
Dylan Ellison <dylancellison@gmail.com>
2019-01-25 13:36:19 -0600

    Remove one more console log

--- client/src/utils/getJSON.js
+++ client/src/utils/getJSON.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const getJSON = url => {
   return axios.get(url)
     .then(response => {
-      console.log("bittrex data is", response.data)
       return response.data
     })
     .catch(function (error) {
